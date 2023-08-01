64a57eed2d72494bcd3badf9967417fb8d7183dc
R.Matsumoto <r.matsumoto159159@gmail.com>
2023-08-01 14:29:35 +0900

    Todoアプリ制作

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TodoController;
-
+use App\Http\Controllers\FolderController;
+use App\Http\Controllers\HomeController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,8 +15,29 @@ use App\Http\Controllers\TodoController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::group(['middleware' => 'auth'], function() {
+    Route::get('/', [HomeController::class,'index'])->name('home');
 
-Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
+    Route::get('/folders/{id}/tasks', [TodoController::class,'index'])->name('tasks.index');
+
+    Route::get('/folders/create', [FolderController::class,'showCreateForm'])->name('folders.create');
+    Route::post('/folders/create', [FolderController::class,'create']);
+
+    Route::get('/folders/{id}/tasks/create', [TodoController::class,'showCreateForm'])->name('tasks.create');
+    Route::post('/folders/{id}/tasks/create', [TodoController::class,'create']);
+
+    Route::get('/folders/{id}/tasks/{task_id}/edit', [TodoController::class,'showEditForm'])->name('tasks.edit');
+    Route::post('/folders/{id}/tasks/{task_id}/edit', [TodoController::class,'edit']);
+});
+Auth::routes();
+/* Route::get('/', [HomeController::class,'index'])->name('home');
+
+Route::get('/folders/{id}/tasks', [TodoController::class,'index'])->name('tasks.index');
+Route::get('/folders/create', [FolderController::class,'showCreateForm'])->name('folders.create');
+Route::post('/folders/create', [FolderController::class,'create']);
+Route::get('/folders/{id}/tasks/create', [TodoController::class,'showCreateForm'])->name('tasks.create');
+Route::post('/folders/{id}/tasks/create', [TodoController::class,'create']);
+Route::get('/folders/{id}/tasks/{task_id}/edit', [TodoController::class,'showEditForm'])->name('tasks.edit');
+Route::post('/folders/{id}/tasks/{task_id}/edit', [TodoController::class,'edit']); */
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
