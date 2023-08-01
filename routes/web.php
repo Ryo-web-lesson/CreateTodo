e92df62610990c8cd337fb2556204c52e5da9f77
R.Matsumoto <r.matsumoto159159@gmail.com>
2023-08-01 11:23:38 +0900

    ルートファイルの修正

--- routes/web.php
+++ routes/web.php
@@ -18,10 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('todos', [TodoController::class,'index']);
-Route::get('todos/create', [TodoController::class,'create']);
-Route::post('todos', [TodoController::class,'store']);
-Route::get('todos/{id}', [TodoController::class,'show']);
-Route::get('todos/{id}/edit', [TodoController::class,'edit']);
-Route::put('todos/{id}', [TodoController::class,'update']);
-Route::delete('todos/{id}', [TodoController::class,'destroy']);
+Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
